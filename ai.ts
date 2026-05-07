3f65fd7d0e4cf84c6aa914fd7c048e117f59199d
Soniyagoswami <soniyagoswami2005@gmail.com>
2026-05-08 02:08:47 +0530

    Upload AI Thumbnail Generator full stack project

---
+++ ai.ts
@@ -0,0 +1,5 @@
+SESSION_SECRET="any_random_string"
+MONGODB_URI="______mongodb_uri______/thumblify"
+GEMINI_API_KEY="____gemini_api_key____" # Requires Google Cloud Credits
+CLOUDINARY_URL="cloudinary://___api_key___:___secret_key___@___cloud_name___"
+NODE_ENV="development"
